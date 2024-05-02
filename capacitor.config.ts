b5d7d655a5a1202499e6f4684fd5242bdbb2331a
javiza <jonathan@gmail.com>
2024-05-02 00:39:41 -0400

    prueba11

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,18 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
-
-
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'javiza',
-  webDir: 'src',
-  plugins: {
-    LiveUpdates: {
-      appId: '042a1261',
-      channel: 'Production',
-      autoUpdateMethod: 'background',
-      maxVersions: 2
-    }
-  }
+  webDir: 'src'
 };
+
 export default config;
